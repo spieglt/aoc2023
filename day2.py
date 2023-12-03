5de8fb31d6991d1ebaebffb901d17cb97397958a
Theron <tspiegl@gmail.com>
2023-12-02 21:28:45 -0600

    day 2

---
+++ day2.py
@@ -0,0 +1,51 @@
+import re
+
+criteria = {
+    'red': 12,
+    'green': 13,
+    'blue': 14,
+}
+
+def day2(input_file):
+    def part1(input_file):
+        f = open(input_file, 'r')
+        sum = 0
+        for i, line in enumerate(f): # each iteration of this loop is a game
+            game_is_possible = True
+            index_of_colon = line.find(':')
+            rounds = line[index_of_colon + 1:].split(';')
+            for round in rounds:
+                for color in ['red', 'green', 'blue']:
+                    colors = re.search(f'(\d+) {color}', round)
+                    if colors != None:
+                        number = colors.group(1)
+                        if int(number) > criteria[color]:
+                            game_is_possible = False
+            if game_is_possible:
+                sum += i + 1
+        return sum
+
+    def part2(input_file):
+        f = open(input_file, 'r')
+        sum = 0
+        for line in f: # each iteration of this loop is a game
+            maximums = {
+                'red': 0,
+                'green': 0,
+                'blue': 0,
+            }
+            index_of_colon = line.find(':')
+            rounds = line[index_of_colon + 1:].split(';')
+            for round in rounds:
+                for color in ['red', 'green', 'blue']:
+                    colors = re.search(f'(\d+) {color}', round)
+                    if colors != None:
+                        number = int(colors.group(1))
+                        if number > maximums[color]:
+                            maximums[color] = number
+            power = maximums['red'] * maximums['green'] * maximums['blue']
+            sum += power
+        return sum
+
+    print('part 1:', part1(input_file))
+    print('part 2:', part2(input_file))
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from day1 import day1
+from day2 import day2
 
 def main():
-    day1('../inputs/1.txt')
+    day1('inputs/1.txt')
+    day2('inputs/2.txt')
 
 main()
