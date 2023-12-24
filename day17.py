9745f7be6b3119056da7246ae16c631750770969
Theron <tspiegl@gmail.com>
2023-12-24 10:07:36 -0600

    17, 18 part 1

--- day17.py
+++ day17.py
@@ -1,13 +1,144 @@
+import copy
+import heapq
+import math
+
+u = 0
+d = 1
+l = 2
+r = 3
+
+class Vertex:
+    def __init__(self, x, y, cost, direction, streak):
+        self.x = x
+        self.y = y
+        self.cost = cost # heat loss for this tile
+        self.direction = direction # direction that was travelled to get to here
+        self.streak = streak # number of times in a row we've travelled this direction
+    dist = math.inf # length of shortest path found to this tile
+    prev = None # (x, y, direction, streak) of previous tile in shortest path
+    def __lt__(self, other):
+        return self.dist < other.dist
+
+def build_graph(str_graph):
+
+    streaks = list(range(11))
+    directions = [copy.deepcopy(streaks) for _ in range(4)]
+    x = [copy.deepcopy(directions) for _ in range(len(str_graph[0]))]
+    graph = [copy.deepcopy(x) for _ in range(len(str_graph))]
+
+    for y in range(len(str_graph)):
+        for x in range(len(str_graph[0])):
+            for d in range(4):
+                for s in range(11):
+                    vertex = Vertex(x, y, int(str_graph[y][x]), d, s)
+                    graph[y][x][d][s] = vertex
+
+    return graph
+
+def get_neighbors(vertex, min_streak, max_streak, graph):
+    neighbors = []
+    neighbors_xy = [None, None, None, None] # up, down, left, right
+    over_min = min_streak <= vertex.streak
+    under_max = vertex.streak < max_streak
+
+    turns = {
+        u: [l, r],
+        d: [r, l],
+        l: [d, u],
+        r: [u, d],
+    }
+    
+    if vertex.y != 0:
+        neighbors_xy[u] = (vertex.x, vertex.y - 1)
+    if vertex.y != len(graph) - 1:
+        neighbors_xy[d] = (vertex.x, vertex.y + 1)
+    if vertex.x != 0:
+        neighbors_xy[l] = (vertex.x - 1, vertex.y)
+    if vertex.x != len(graph[0]) - 1:
+        neighbors_xy[r] = (vertex.x + 1, vertex.y)
+
+    if under_max:
+        straight_neighbor_xy = neighbors_xy[vertex.direction]
+        if straight_neighbor_xy:
+            straight_neighbor = graph[straight_neighbor_xy[1]][straight_neighbor_xy[0]][vertex.direction][vertex.streak + 1]
+            neighbors.append(straight_neighbor)
+
+    if over_min:
+        left_neighbor_direction = turns[vertex.direction][0]
+        left_neighbor_xy = neighbors_xy[left_neighbor_direction]
+        if left_neighbor_xy:
+            left_neighbor = graph[left_neighbor_xy[1]][left_neighbor_xy[0]][left_neighbor_direction][1]
+            neighbors.append(left_neighbor)
+        right_neighbor_direction = turns[vertex.direction][1]
+        right_neighbor_xy = neighbors_xy[right_neighbor_direction]
+        if right_neighbor_xy:
+            right_neighbor = graph[right_neighbor_xy[1]][right_neighbor_xy[0]][right_neighbor_direction][1]
+            neighbors.append(right_neighbor)
+
+    return neighbors
+
+def dijkstra(graph, min_streak, max_streak, destination):
+    source = graph[0][0][1][0] # set initial direction to down so it doesn't keep us from backtracking. streak == 0.
+    source.dist = 0
+    queue = [source]
+    while queue != []:
+        shortest = heapq.heappop(queue)
+        if shortest.x == destination[0] and shortest.y == destination[1]:
+            return shortest
+        for neighbor in get_neighbors(shortest, min_streak, max_streak, graph):
+            this_dist = shortest.dist + neighbor.cost
+            if this_dist < neighbor.dist:
+                neighbor.dist = this_dist
+                neighbor.prev = (shortest.x, shortest.y, shortest.direction, shortest.streak)
+                heapq.heappush(queue, neighbor)
+
+
+def retrace(target, graph, graph_copy):
+    previous = target
+    total_cost = 0
+    while previous.prev:
+        graph_copy[previous.y][previous.x] = '\x1b[1;31;40m' + '#' + '\x1b[0m'
+        total_cost += previous.cost
+        (x, y, d, s) = previous.prev
+        previous = graph[y][x][d][s]
+    return total_cost
+
 
 def solve(input_file):
     f = open(input_file, 'r')
     lines = [list(l.strip()) for l in f.readlines()]
 
+    # lines = '''
+    #     2413432311323
+    #     3215453535623
+    #     3255245654254
+    #     3446585845452
+    #     4546657867536
+    #     1438598798454
+    #     4457876987766
+    #     3637877979653
+    #     4654967986887
+    #     4564679986453
+    #     1224686865563
+    #     2546548887735
+    #     4322674655533'''.split('\n')
+    # lines = [list(l.strip()) for l in lines if l]
+
+    def find_shortest_path(min_streak, max_streak):
+        graph = build_graph(lines)
+        destination = (len(lines[0]) - 1, len(lines) - 1)
+        target = dijkstra(graph, min_streak, max_streak, destination)
+        graph_copy = copy.deepcopy(lines)
+        total_cost = retrace(target, graph, graph_copy)
+        for line in graph_copy:
+            print(''.join(line))
+        return target.dist
+
     def part1():
-        pass
+        return find_shortest_path(0, 3)
 
     def part2():
-        pass
+        return find_shortest_path(4, 10)
 
     print('day 17')
     print('part 1:', part1())
--- day18.py
+++ day18.py
@@ -1,17 +1,201 @@
+import copy
+
+u = 'U'
+d = 'D'
+l = 'L'
+r = 'R'
+
+def get_dimensions(directions, lengths):
+    x_min, x_max, y_min, y_max = 0, 0, 0, 0
+    x, y = 0, 0
+    for i, direction in enumerate(directions):
+        if direction == u:
+            y -= lengths[i]
+        if direction == d:
+            y += lengths[i]
+        if direction == l:
+            x -= lengths[i]
+        if direction == r:
+            x += lengths[i]
+        if x < x_min:
+            x_min = x
+        if x > x_max:
+            x_max = x
+        if y < y_min:
+            y_min = y
+        if y > y_max:
+            y_max = y
+    return (x_min, x_max, y_min, y_max)
+
+def go(cursor, direction, length, land):
+    length += 1
+    x, y = cursor[0], cursor[1]
+    if direction == 'U':
+        for i in range(length):
+            land[y-i][x] = '#'
+        return (x, y-i)
+    if direction == 'D':
+        for i in range(length):
+            land[y+i][x] = '#'
+        return (x, y+i)
+    if direction == 'L':
+        for i in range(length):
+            land[y][x-i] = '#'
+        return (x-i, y)
+    if direction == 'R':
+        for i in range(length):
+            land[y][x+i] = '#'
+        return (x+i, y)
+
+def paint(cursor, direction, length, land):
+    length += 1
+    x, y = cursor[0], cursor[1]
+    if direction == 'U':
+        for i in range(length):
+            if land[y-i][x+1] == ' ':
+                land[y-i][x+1] = 'O'
+        return (x, y-i)
+    if direction == 'D':
+        for i in range(length):
+            if land[y+i][x-1] == ' ':
+                land[y+i][x-1] = 'O'
+        return (x, y+i)
+    if direction == 'L':
+        for i in range(length):
+            if land[y-1][x-i] == ' ':
+                land[y-1][x-i] = 'O'
+        return (x-i, y)
+    if direction == 'R':
+        for i in range(length):
+            if land[y+1][x+i] == ' ':
+                land[y+1][x+i] = 'O'
+        return (x+i, y)
+
+def get_lake_size(directions, lengths):
+    
+        # get dimensions needed for lake
+        dimensions = get_dimensions(directions, lengths)
+        width = abs(dimensions[0]) + abs(dimensions[1]) + 1
+        height = abs(dimensions[2]) + abs(dimensions[3]) + 1
+
+        #  make space for the lake
+        cursor = (abs(dimensions[0]), abs(dimensions[2]))
+        row = [' ' for _ in range(width)]
+        land = [copy.deepcopy(row) for _ in range(height)]
+
+        # draw the border path
+        for i, direction in enumerate(directions):
+            cursor = go(cursor, direction, lengths[i], land)
+        assert cursor == (abs(dimensions[0]), abs(dimensions[2]))
+
+        # mark the inside of the path with O
+        for i, direction in enumerate(directions):
+            cursor = paint(cursor, direction, lengths[i], land)
+        assert cursor == (abs(dimensions[0]), abs(dimensions[2]))
+        
+        land[cursor[1]][cursor[0]] = 'X'
+
+        # fill in the rest of the interior
+        for y in range(len(land)):
+            interior = False
+            for x in range(len(land[0])):
+                if interior and land[y][x] == ' ':
+                    land[y][x] = 'O'
+                if land[y][x] == 'O':
+                    interior = True
+                if land[y][x] not in 'O ':
+                    interior = False
+
+        # count all interior and border tiles
+        total_lava = 0
+        for r in land:
+            total_lava += r.count('#')
+            total_lava += r.count('O')
+            print(''.join(r))
+        return total_lava
+
+def get_points(directions, lengths):
+    points = {}
+    x, y = 0, 0
+    for i, direction in enumerate(directions):
+        new_points = []
+        if direction == 'U':
+            for j in range(lengths[i] + 1):
+                new_points.append((x, y-j))
+            y -= j
+        if direction == 'D':
+            for j in range(lengths[i] + 1):
+                new_points.append((x, y+j))
+            y += j
+        if direction == 'L':
+            for j in range(lengths[i] + 1):
+                new_points.append((x-j, y))
+            x -= j
+        if direction == 'R':
+            for j in range(lengths[i] + 1):
+                new_points.append((x+j, y))
+            x += j
+        for point in new_points:
+            xs_for_y = points.get(point[1])
+            if not xs_for_y:
+                points[point[1]] = {point[0]}
+            else:
+                xs_for_y.add(point[0])
+    print(len(points))
+    return points
+
+def new_get_lake_size(directions, lengths):
+    total = 0
+    points = get_points(directions, lengths)
+    print(f'points: {points}')
+    dimensions = get_dimensions(directions, lengths)
+    for y in range(dimensions[2], dimensions[3] + 5):
+        row = points.get(y)
+        if row:
+            row_total = 0
+            sorted_row = sorted(row)
+            interior = False
+            for i, x in enumerate(sorted_row):
+
+                
+
+
+
+                row_total += 1 # add current x, even if on horizontal line
+                print(f'added one for ({x}, {y})')
+                if i + 1 < len(sorted_row): # if there is no next x, there can't be a closing wall
+                    if sorted_row[i+1] - x > 1: # if there's a gap between this x and the next one, we're crossing a wall, detect inside/outside
+                        interior = not interior
+                        if interior:
+                            row_total += sorted_row[i+1] - x
+                            print(f'adding {row_total} for ({x}, {y}) to ({sorted_row[i+1]}, {y})')
+                    # else: determine wall type: s or u or A. get wall beginning and end. 
+            print(f'total for row {y}: {row_total}')
+            total += row_total
+    return total
 
 def solve(input_file):
     f = open(input_file, 'r')
-    lines = [list(l.strip()) for l in f.readlines()]
+    lines = [l.strip() for l in f.readlines()]
+    lines = [l.split(' ') for l in lines]
+    directions = [l[0] for l in lines]
+    lengths = [int(l[1]) for l in lines]
+    colors = [l[2] for l in lines]
 
     def part1():
-        pass
+        print('new:', new_get_lake_size(directions, lengths))
+        return get_lake_size(directions, lengths)
+
 
     def part2():
-        pass
+        length_map = {'0': 'R', '1': 'D', '2': 'L', '3': 'U'}
+        new_directions = [length_map[c[7]] for c in colors]
+        new_lengths = [int(c[2:7], 16) for c in colors]
+        return new_get_lake_size(new_directions, new_lengths)
 
     print('day 18')
     print('part 1:', part1())
-    print('part 2:', part2())
+    # print('part 2:', part2())
 
 if __name__ == '__main__':
     solve('inputs/18.txt')
